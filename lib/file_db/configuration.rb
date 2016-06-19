6154da924a3ba4147a520d1c8bebf2f9368e94c8
robst <robertst81@gmail.com>
2016-06-19 12:45:44 +0200

    configuration for file_db

---
+++ lib/file_db/configuration.rb
@@ -0,0 +1,19 @@
+module FileDb
+  module Configuration
+    def self.configure options = {}
+      @configuration = default_options.merge(options)
+    end
+
+    def self.configured option
+      configuration[option]
+    end
+
+    def self.configuration
+      @configuration ||= default_options
+    end
+
+    def self.default_options
+      { data_directory: 'data'  }
+    end
+  end
+end
