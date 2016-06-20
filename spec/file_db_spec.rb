65aae76fda0110b24cca07ea9fb2b0795055715d
robst <robertst81@gmail.com>
2016-06-20 19:35:35 +0200

    first basic tests

--- spec/file_db_spec.rb
+++ spec/file_db_spec.rb
@@ -5,6 +5,29 @@ describe FileDb do
     expect(FileDb::VERSION).not_to be nil
   end
 
+  describe 'classes acting as FileDb::Model' do
+    before do
+      class User < FileDb::Model
+        columns :name, :test
+      end
+    end
+     
+    let(:user) { User.new name: 'Tester' }
+    it 'should have accessors' do
+      expect(user.respond_to?(:name)).to be_truthy
+      expect(user.respond_to?(:name=)).to be_truthy
+      expect(user.respond_to?(:test)).to be_truthy
+      expect(user.respond_to?(:test=)).to be_truthy
+    end
+
+    it 'should return user as table_name' do
+      expect(User.table_name).to eq('user')
+    end
+
+    it 'set params hash to accessors' do
+      expect(user.name).to eq('Tester')
+    end
+  end
   it 'does something useful' do
     expect(false).to eq(true)
   end
