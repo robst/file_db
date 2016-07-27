32e1a898613978d9ab2da955b444b8df5b637de8
robst <robertst81@gmail.com>
2016-07-27 05:15:04 +0200

    changed database modul to singleton

--- lib/file_db/database.rb
+++ lib/file_db/database.rb
@@ -1,20 +1,25 @@
+require 'singleton'
 module FileDb
-  module Database
+  class Database
+    include Singleton
+
     def self.database_check!
-      create_data_directory! unless exist_data_directory?
-    end
-    
-    private
-    def self.data_directory
-      Configuration.configured(:data_directory)
+      instance.create_data_directory! unless instance.exist_data_directory?
     end
 
-    def self.exist_data_directory?
+    def exist_data_directory?
       File.exist? data_directory
     end
 
-    def self.create_data_directory!
+    def create_data_directory!
       Dir.mkdir data_directory
     end
+
+    private
+
+    def data_directory
+      Configuration.configured(:data_directory)
+    end
   end
+
 end
