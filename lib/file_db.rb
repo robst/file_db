27b154a74b81a6324ca0d76b709b37b9158d23a3
robst <robertst81@gmail.com>
2016-11-30 21:54:53 +0100

    reorganized includes

--- lib/file_db.rb
+++ lib/file_db.rb
@@ -1,13 +1,13 @@
-require "csv"
 require "file_db/version"
 require "file_db/system/check"
+require "file_db/system/database"
+require "file_db/system/table"
 require "file_db/configuration"
-require "file_db/database"
+require "file_db/database_manager"
 require "file_db/columns"
 require "file_db/table"
 require "file_db/data"
 require "file_db/query"
-require "file_db/convert"
 require "file_db/model"
 
 module FileDb
