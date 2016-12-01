b9f5c551d943dfb18e2954c2a50805f35ad6ef0a
robst <robertst81@gmail.com>
2016-12-01 21:06:24 +0100

    drop and create database moved to database manager

--- lib/file_db/database_manager.rb
+++ lib/file_db/database_manager.rb
@@ -8,5 +8,13 @@ module FileDb
     def initialize
       @database = System::Database.new Configuration.configured(:data_directory)
     end
+
+    def drop_database!
+      database.drop_data_directory!
+    end
+
+    def create_database_if_not_exist!
+      database.create_database_if_not_exist!
+    end
   end
 end
