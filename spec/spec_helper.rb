61b09c5457175b7825f38a85b690ba3f5a0adad5
robst <robertst81@gmail.com>
2016-12-01 21:14:07 +0100

    changed calls for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,20 +4,17 @@ require 'file_db'
 
 RSpec.configure do |config|
   config.before(:suite) do
+    FileDb::Configuration.configure data_directory: 'data'
+
     class User < FileDb::Model
       columns :name, :test
     end
-    FileDb::Configuration.configure data_directory: 'data'
-    FileDb::System::Check.run!
+
     User.create name: 'max', test: 'test'
     User.create name: 'tester', test: 'tests'
   end
 
   config.after(:suite) do
-    File.delete File.join(
-      FileDb::Configuration.configured(:data_directory),
-      "#{User.table_name}.csv"
-    )
-    Dir.delete(FileDb::Configuration.configured :data_directory)
+    FileDb::DatabaseManager.instance.drop_database!
   end
 end
